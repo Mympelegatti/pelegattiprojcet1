50c266d6def6aaaa44855389bb1c662277d2c2d8
mym <izabellapelegattipadia@gmail.com>
2024-05-28 08:52:01 -0300

    Update and rename README.md to sketch.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# pelegattiprojcet1
---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function filmesParaAssistir(diaDaSemana, genero) {
+
+  // ...
+
+  return sugestaoFilme;
+}
+function modoNoturno(horario) {
+  if (horario > 18) {
+    console.log("Você precisa ligar o modo noturno!");
+  } else {
+    console.log("Modo noturno não é necessário neste momento.");
+  }
+}
+
+modoNoturno(15);
+modoNoturno(20);
